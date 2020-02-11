526b0327d41cdc679f71b3074920dc7ffa13086d
oxillo <oxillo@hotmail.com>
2020-02-11 22:14:58 +0100

    remove confirmation

--- scripts/ci/msys2/install.sh
+++ scripts/ci/msys2/install.sh
@@ -1,1 +1,1 @@
-pacman --needed -Syu ${MINGW_PACKAGE_PREFIX}-ninja ${MINGW_PACKAGE_PREFIX}-cmake ${MINGW_PACKAGE_PREFIX}-gtest ${MINGW_PACKAGE_PREFIX}-lcov
+pacman --needed --noconfirm -Syu ${MINGW_PACKAGE_PREFIX}-ninja ${MINGW_PACKAGE_PREFIX}-cmake ${MINGW_PACKAGE_PREFIX}-gtest ${MINGW_PACKAGE_PREFIX}-lcov
