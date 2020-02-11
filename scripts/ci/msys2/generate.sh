542b56d445e1d02ec1698c95dad78ba0b3e90e76
oxillo <oxillo@hotmail.com>
2020-02-11 20:37:49 +0100

    MSYS2 build on appveyor

---
+++ scripts/ci/msys2/generate.sh
@@ -0,0 +1,12 @@
+mkdir build
+cd build
+cmake .. -G"Ninja" \
+    -DWORDSIZE=x64 \
+    -DLIBED247_VERSION=1.0.0 \
+    -DLIBXML2_PATH=${MINGW_PREFIX} \
+    -DXMLLINT_EXE=xmllint.exe \
+    -DLCOV_PATH=${MINGW_PREFIX} \
+    -DLCOV_EXE=lcov.exe \
+    -DGENHTML_EXE=genhtml.exe \
+    -DCMAKE_INSTALL_PREFIX= ../install
+cd ..
