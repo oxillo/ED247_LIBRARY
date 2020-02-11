6218d4cd48634850746677564079344c2c4ddd99
oxillo <oxillo@hotmail.com>
2020-02-11 23:20:27 +0100

    generate dummy dependencies.cmake

--- scripts/ci/msys2/generate.sh
+++ scripts/ci/msys2/generate.sh
@@ -1,6 +1,13 @@
 mkdir build
 mkdir install
 cd build
+#
+#cmake_variables=(COMPILER WORDSIZE LIBED247_VERSION DOXYGEN_PATH LCOV_PATH LCOV_EXE GENHTML_EXE GTEST_PATH LIBXML2_PATH XMLLINT_EXE)
+#            cmake_variables+=(MINGW_PATH)
+#            "SET(WORDSIZE x64)"
+
+echo   > dependencies.cmake
+
 cmake .. -G"Ninja" \
     -DWORDSIZE=x64 \
     -DLIBED247_VERSION=1.0.0 \
