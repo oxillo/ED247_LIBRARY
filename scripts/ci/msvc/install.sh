51b4859c88f0630cd5042be57ee51b989d729103
oxillo <oxillo@hotmail.com>
2020-02-16 07:07:10 +0100

    trying to build libxml2

--- scripts/ci/msvc/install.sh
+++ scripts/ci/msvc/install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+#Download LibXML2 source
 VER=2.9.10
+wget -nv https://gitlab.gnome.org/GNOME/libxml2/-/archive/v$VER/libxml2-v$VER.zip
 
-wget -nv https://gitlab.gnome.org/GNOME/libxml2/-/archive/v$VER/libxml2-v$VER.tar.gz
-
-tar xzf libxml2-v$VER.tar.gz
+unzip libxml2-v$VER.zip
 mv libxml2-v$VER libxml2
 rm libxml2-v$VER.tar.gz
 
 pushd libxml2/win32
 cscript configure.js compiler=msvc
+nmake /f Makefile.msvc
 popd
+
+#Download Ninja
+wget -nv https://github.com/ninja-build/ninja/releases/download/v1.10.0/ninja-win.zip
+unzip ninja-win.zip
