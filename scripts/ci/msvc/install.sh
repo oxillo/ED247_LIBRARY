23e2ce4f79cec6439ff4ea00de5047ec971d5459
oxillo <oxillo@hotmail.com>
2020-02-16 15:17:49 +0100

    Conan

--- scripts/ci/msvc/install.sh
+++ scripts/ci/msvc/install.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-choco install cmake
-choco install ninja
-choco install conan
-
-conan install .
-
-#Download LibXML2 source
-VER=2.9.10
-wget -nv https://gitlab.gnome.org/GNOME/libxml2/-/archive/v$VER/libxml2-v$VER.zip
-
-unzip libxml2-v$VER.zip
-dir
-mv libxml2-v$VER libxml2
-rm libxml2-v$VER.zip
-
-pushd libxml2/win32
-cscript configure.js compiler=msvc
-nmake /f Makefile.msvc
-popd
+# choco install cmake 
+#choco install ninja
+#choco install conan
 
