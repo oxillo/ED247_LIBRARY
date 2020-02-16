389a68f60510915d37b0fd5a3660573d4a6d1ea6
oxillo <oxillo@hotmail.com>
2020-02-16 04:57:42 +0100

    install libxml2

--- scripts/ci/msys2/install.sh
+++ scripts/ci/msys2/install.sh
@@ -1,1 +1,1 @@
-pacman --needed --noconfirm -Syu ${MINGW_PACKAGE_PREFIX}-ninja ${MINGW_PACKAGE_PREFIX}-cmake ${MINGW_PACKAGE_PREFIX}-gtest ${MINGW_PACKAGE_PREFIX}-lcov
+pacman --needed --noconfirm -Syu ${MINGW_PACKAGE_PREFIX}-ninja ${MINGW_PACKAGE_PREFIX}-cmake ${MINGW_PACKAGE_PREFIX}-gtest ${MINGW_PACKAGE_PREFIX}-lcov ${MINGW_PACKAGE_PREFIX}-libxml2
