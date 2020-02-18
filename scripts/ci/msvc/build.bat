2fe9c9755054539851135571fb5354201f603817
oxillo <oxillo@hotmail.com>
2020-02-18 06:13:00 +0100

    change vcvarsall

--- scripts/ci/msvc/build.bat
+++ scripts/ci/msvc/build.bat
@@ -5,6 +5,11 @@ set
 where ninja
 where cmake
 where nmake
-vcvarsall.bat
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
 cmake .. -G "Ninja"
 popd
+
+rem C:\Program Files\Meson\ninja.EXE
+rem C:\ProgramData\chocolatey\bin\ninja.exe
+rem C:\Program Files (x86)\CMake\bin\cmake.exe
