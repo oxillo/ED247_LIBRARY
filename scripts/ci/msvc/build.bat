308fc856801cf666cdfbf3bc279e847663bd3150
oxillo <oxillo@hotmail.com>
2020-02-19 22:04:41 +0100

    msvc

--- scripts/ci/msvc/build.bat
+++ scripts/ci/msvc/build.bat
@@ -1,14 +1,29 @@
 mkdir build
 pushd build 
 conan install .. 
-set
-where ninja
-where cmake
-where nmake
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %WORDSIZE%
+
 cmake .. -G "Ninja"
+if %errorlevel% NEQ 0 (
+    echo "Cmake cannot configure the project"
+    exit /b 1
+)
 popd
+
+cmake --build build --target all
+if %errorlevel% NEQ 0 (
+    echo "Cmake cannot build the project"
+    exit /b 1
+)
+
+
+cmake --build build --target install
+if %errorlevel% NEQ 0 (
+    echo "Cmake cannot install the project"
+    exit /b 1
+)
+
 
 rem C:\Program Files\Meson\ninja.EXE
 rem C:\ProgramData\chocolatey\bin\ninja.exe
